57002557c7ad9b3e7ccff70e323b2ea277353f86
Aman Chhabra <aman.chhabra1@gmail.com>
2018-05-04 16:25:30 -0700

    File cleanup

--- src/repl.js
+++ src/repl.js
@@ -5,10 +5,3 @@ const dark = new SparkAfterDark();
 const repl = require("repl");
 const replServer = repl.start();
 replServer.context.dark = dark;
-
-dark.spark.memberships
-  .list({
-    roomId: "cdd89a90-4aff-11e7-af0f-bf62e4939ba3",
-    personEmail: "aman.chhabra1+colonel@gmail.com"
-  })
-  .then(rooms => console.log(JSON.stringify(rooms.items)));
